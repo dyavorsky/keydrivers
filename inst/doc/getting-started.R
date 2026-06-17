dd5d81491addef9cd7177771e196faebc7710947
Dan Yavorsky <dyavorsky@gmail.com>
2026-06-16 22:23:02 -0700

    Update vignette

--- inst/doc/getting-started.R
+++ inst/doc/getting-started.R
@@ -87,3 +87,46 @@ plot(result_boot,
 # library(ggplot2)
 # plot(result) + ggtitle("Key Drivers of Fuel Efficiency")
 
+
+## -----------------------------------------------------------------------------
+#| eval: false
+# library(ggplot2)
+# 
+# for (v in c("disp", "hp", "wt", "drat")) {
+#   p <- ggplot(mtcars, aes(x = .data[[v]], y = mpg)) +
+#     geom_point() +
+#     geom_smooth(method = "loess", se = TRUE) +
+#     geom_smooth(method = "lm", se = FALSE, color = "red", linetype = "dashed") +
+#     ggtitle(v)
+#   print(p)
+# }
+
+
+## -----------------------------------------------------------------------------
+#| eval: false
+# library(randomForest)
+# library(pdp)
+# 
+# rf_model <- randomForest(mpg ~ disp + hp + wt + drat, data = mtcars, ntree = 1000)
+# partial(rf_model, pred.var = "wt", plot = TRUE, rug = TRUE)
+
+
+## -----------------------------------------------------------------------------
+#| eval: false
+# library(mgcv)
+# 
+# m_lm  <- lm(mpg ~ disp + hp + wt + drat, data = mtcars)
+# m_gam <- gam(mpg ~ s(disp) + s(hp) + s(wt) + s(drat), data = mtcars)
+# 
+# summary(m_lm)$r.squared
+# summary(m_gam)$r.sq   # materially higher = non-linearity matters
+
+
+## -----------------------------------------------------------------------------
+#| eval: false
+# partial(rf_model, pred.var = c("wt", "hp"), plot = TRUE, chull = TRUE)
+# 
+# # Confirm with a formal test in the linear model
+# m_int <- lm(mpg ~ disp + hp + wt + drat + wt:hp, data = mtcars)
+# anova(m_lm, m_int)
+
